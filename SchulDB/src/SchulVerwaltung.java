3e9f354720a480ca8c62a436da0a27744ffc7510
Microstar301 <microstar301@gmail.com>
2019-04-22 20:24:36 +0200

    third

--- SchulDB/src/SchulVerwaltung.java
+++ SchulDB/src/SchulVerwaltung.java
@@ -1,15 +1,45 @@
 import de.thm.schule.Lehrer;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 
 public class SchulVerwaltung {
     public static void main(String[] args){
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("THM");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
 
-        Lehrer l = entityManager.find(de.thm.schule.Lehrer.class,1);
-        System.out.println(l.getNachname()+" "+l.getVorname());
+        entityTransaction.begin();
 
+        ArrayList<Lehrer> lehrerArrayList = new ArrayList<>();
+        Lehrer l;
+        int c = 1;
+        boolean done = false;
+        while( !done) {
+            l = entityManager.find(Lehrer.class, c);
+            if (l != null) {
+                lehrerArrayList.add(l);
+            }else {
+                done = true;
+            }
+            c++;
+        }
+        lehrerArrayList.get(1).setNachname(null);
+        for(int i=0;i<lehrerArrayList.size();i++){
+            System.out.print(lehrerArrayList.get(i).getPnr()+". ");
+            if(lehrerArrayList.get(i).getTitel() != null)
+                System.out.print(lehrerArrayList.get(i).getTitel()+" ");
+            System.out.print(lehrerArrayList.get(i).getVorname()+" ");
+            System.out.print(lehrerArrayList.get(i).getNachname()+" ");
+            System.out.print(lehrerArrayList.get(i).getGebdatum()+" ");
+            System.out.print(lehrerArrayList.get(i).getSchuleintritt()+" ");
+            System.out.print(lehrerArrayList.get(i).getStufe()+" \n");
+        }
+
+        l = entityManager.find(Lehrer.class, 1);
+        entityManager.persist(l);
+        l.setNachname("Schmittengrund");
+        entityTransaction.commit();
 
         entityManager.close();
         entityManagerFactory.close();
